127077c293225e4f4fb608fc2529c72d3cef92aa
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-07-19 21:08:59 +0330

    add : mini calculator

---
+++ calculator/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var operation string
+	fmt.Print("Please select an operation: +, -, *, / : ")
+	fmt.Scanln(&operation)
+
+	var num1 int
+	fmt.Print("Please input the first number: ")
+	fmt.Scanln(&num1)
+
+	var num2 int
+	fmt.Print("Please input the second number: ")
+	fmt.Scanln(&num2)
+
+	switch operation {
+	case "+":
+
+		fmt.Print("Result: ", "\n", num1+num2)
+
+	case "-":
+		fmt.Print("Result: ", "\n", num1-num2)
+
+	case "*":
+		fmt.Print("Result: ", "\n", num1*num2)
+
+	case "/":
+		fmt.Print("Result: ", "\n", num1/num2)
+
+	default:
+		fmt.Println("Invalid operation selected. Please try again!")
+
+	}
+}
